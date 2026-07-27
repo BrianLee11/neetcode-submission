be72567607175601cff7775b67a897edff3e3abf
Brian J. Lee <134612285+BrianLee11@users.noreply.github.com>
2026-07-27 18:53:16 -0400

    Add: isomorphic-strings - submission-3

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-3.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dictionary_s = defaultdict(list)
+        dictionary_t = defaultdict(list)
+
+        for index, character in enumerate(s):
+            dictionary_s[character].append(index)
+    
+        for index, character in enumerate(t):
+            dictionary_t[character].append(index)
+        
+        set_tuple_s = set(tuple(value) for value in dictionary_s.values())
+        set_tuple_t = set(tuple(value) for value in dictionary_t.values())        
+
+        return set_tuple_s == set_tuple_t
