6176ffc2521e98285ea5e06e2919a94c704c3275
Brian J. Lee <134612285+BrianLee11@users.noreply.github.com>
2026-07-30 13:18:50 -0400

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-4

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-4.py
@@ -0,0 +1,82 @@
+# LeetCode
+# 3105. Longest Strictly Increasing or Strictly Decreasing Subarray
+# https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray/description/
+
+# NeetCode
+# https://neetcode.io/problems/longest-strictly-increasing-or-strictly-decreasing-subarray/question?list=allNC
+
+# Start date/time: 2026-07-30 12:49
+# End date/time: 2026-07-30 13:09:10
+
+from typing import List
+from enum import Enum
+
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        # trivial cases: when length is 0 or 1
+        length_nums = len(nums)
+
+        if length_nums == 0:
+            return 0
+
+        if length_nums == 1:
+            return 1
+
+        # Enum for clearer state representation
+        class Status(Enum):
+            INCREASING = 1
+            DECREASING = 2
+            CONSTANT = 3
+
+        # initialize
+        first_number = nums[0]
+        second_number = nums[1]
+        previous_status = Status.CONSTANT
+        current_count = 0
+        max_count = 0
+
+        # the first relationship
+        if first_number < second_number:
+            previous_status = Status.INCREASING
+            current_count += 2
+        elif first_number > second_number:
+            previous_status = Status.DECREASING
+            current_count += 2
+        elif first_number == second_number:
+            current_count += 1
+        max_count = current_count
+
+        # trivial case
+        if length_nums == 2:
+            return max_count
+
+        # iterate the rest of the relationship
+        for index in range(1, len(nums) - 1, 1):
+            first_number = nums[index]
+            second_number = nums[index + 1]
+            current_status = Status.CONSTANT
+
+            # compute current status
+            if first_number < second_number:
+                current_status = Status.INCREASING
+
+            elif first_number > second_number:
+                current_status = Status.DECREASING
+
+            elif first_number == second_number:
+                current_count = 1
+
+            # compare current status with previous status
+            if current_status != Status.CONSTANT:
+                if previous_status != current_status:
+                    current_count = 2                # reset the count
+                    previous_status = current_status # reset the relationship
+
+                else:
+                    current_count += 1 # increment count, if the same status
+
+                # update the maximum value
+                if current_count > max_count:
+                    max_count = current_count
+
+        return max_count
